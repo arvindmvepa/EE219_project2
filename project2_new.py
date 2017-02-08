1f41ba6bd96663bca3ba41664842f48d773e544c
mertalay <mrtalay@Mert-MacBook-Pro.local>
2017-02-07 20:13:59 -0800

    best version,shorter,faster

--- project2_new.py
+++ project2_new.py
@@ -1,60 +1,43 @@
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction import text
+import warnings
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction import text
+
+warnings.filterwarnings("ignore")
 
 #Part a
-comp_categories = [['comp.graphics'],['comp.os.ms-windows.misc'],['comp.sys.ibm.pc.hardware'],['comp.sys.mac.hardware']]
-rec_categories = [['rec.autos'],['rec.motorcycles'],['rec.sport.baseball'],['rec.sport.hockey']]
+comp_categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware','comp.sys.mac.hardware']
+rec_categories = ['rec.autos','rec.motorcycles','rec.sport.baseball','rec.sport.hockey']
 
-comp_train = {}
-comp_test = {}
-for category in comp_categories:
-    comp_train["{0}".format(category)] = fetch_20newsgroups(subset='train', categories=category, shuffle=True, random_state=42)
-    comp_test["{0}".format(category)] = fetch_20newsgroups(subset='test', categories=category, shuffle=True, random_state=42)
+comp_train = fetch_20newsgroups(subset='train', categories=comp_categories, shuffle=True, random_state=42)
+comp_test = fetch_20newsgroups(subset='test', categories=comp_categories, shuffle=True, random_state=42)
+rec_train = fetch_20newsgroups(subset='train', categories=rec_categories, shuffle=True, random_state=42)
+rec_test = fetch_20newsgroups(subset='test', categories=rec_categories, shuffle=True, random_state=42)
 
-rec_train = {}
-rec_test = {}
-for category in rec_categories:
-    rec_train["{0}".format(category)] = fetch_20newsgroups(subset='train', categories=category, shuffle=True, random_state=42)
-    rec_test["{0}".format(category)] = fetch_20newsgroups(subset='test', categories=category, shuffle=True, random_state=42)
+print("Training Set - Computer Technology: %s Recreation: %s" %(comp_train.filenames.shape[0],rec_train.filenames.shape[0]))
+print("Test Set - Computer Technology: %s Recreation: %s" %(comp_test.filenames.shape[0],rec_test.filenames.shape[0]))
 
-comp_train_total = comp_test_total = 0
-comp_train_counts = []
-comp_test_counts = []
-for category in comp_categories:
-    comp_train_total += comp_train["{0}".format(category)].filenames.shape[0]
-    comp_train_counts.append(comp_train["{0}".format(category)].filenames.shape[0])
-    comp_test_total += comp_test["{0}".format(category)].filenames.shape[0]
-    comp_test_counts.append(comp_test["{0}".format(category)].filenames.shape[0])
+comp_train_list=comp_train.target.tolist()
+rec_train_list=rec_train.target.tolist()
+comp_test_list=comp_test.target.tolist()
+rec_test_list=rec_test.target.tolist()
 
-rec_train_total = rec_test_total = 0
-rec_train_counts = []
-rec_test_counts = []
-for category in rec_categories:
-    rec_train_total += rec_train["{0}".format(category)].filenames.shape[0]
-    rec_train_counts.append(rec_train["{0}".format(category)].filenames.shape[0])
-    rec_test_total += rec_test["{0}".format(category)].filenames.shape[0]
-    rec_test_counts.append(rec_test["{0}".format(category)].filenames.shape[0])
-
-print("Training Set - Computer Technology: %s Recreation: %s" %(comp_train_total,rec_train_total))
-print("Test Set - Computer Technology: %s Recreation: %s" %(comp_test_total,rec_test_total))
+train_counts = [[comp_train_list.count(x)] for x in set(comp_train_list)] + [[rec_train_list.count(x)] for x in set(rec_train_list)]
+test_counts = [[comp_test_list.count(x)] for x in set(comp_test_list)] + [[rec_test_list.count(x)] for x in set(rec_test_list)]
 
 objects = ('graphics', 'windows', 'ibm', 'mac', 'autos', 'cycles','baseball','hockey')
 y_pos = np.arange(len(objects))
 
-train_counts = comp_train_counts + rec_train_counts
-test_counts = comp_test_counts + rec_test_counts
-
-plt.bar(y_pos, train_counts, align='center', alpha=0.5)
+plt.bar(y_pos, np.array(train_counts), align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Counts')
 plt.title('Counts for Training Set')
 plt.tight_layout() 
 plt.show()
 
-plt.bar(y_pos, test_counts, align='center', alpha=0.5)
+plt.bar(y_pos, np.array(test_counts), align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Counts')
 plt.title('Counts for Test Set')
