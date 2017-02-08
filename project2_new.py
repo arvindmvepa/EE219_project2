dff86f5364d461d7637ac77afd2624453c594a11
mertalay <mrtalay@Mert-MacBook-Pro.local>
2017-02-07 18:02:13 -0800

    same

--- project2_new.py
+++ project2_new.py
@@ -61,6 +61,7 @@ plt.title('Counts for Test Set')
 plt.tight_layout()  
 plt.show()
 
+
 #Part b
 categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
 
