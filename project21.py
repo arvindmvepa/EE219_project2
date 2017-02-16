ecad40798308f97e89acf656c0b2320ee33cbdeb
arvindmvepa <arvind.m.vepa@gmail.com>
2017-02-15 22:23:06 -0800

    removed comments

--- project21.py
+++ project21.py
@@ -8,8 +8,7 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import sklearn.linear_model as sk
 from sklearn.multiclass import OneVsOneClassifier
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn import preprocessing
-from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing, cross_validation
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction import text
 from sklearn.decomposition import TruncatedSVD
@@ -17,6 +16,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report, confusion_matrix, roc_curve
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC, SVC
+from statsmodels.discrete.discrete_model import Logit
 from nltk import SnowballStemmer
 from collections import Counter
 from collections import defaultdict
@@ -24,14 +24,15 @@ from collections import defaultdict
 warnings.filterwarnings("ignore")
 
 ### PRINT ALL 20 NEWSGROUPS ###
-newsgroups_train = fetch_20newsgroups(subset='train')
-print("Names of All Newsgroups: " + str(list(newsgroups_train.target_names)))
-print('\n')
+# newsgroups_train = fetch_20newsgroups(subset='train')
+# print("Names of All Newsgroups: " + str(list(newsgroups_train.target_names)))
+# print('\n')
 
 ### PART A ###
 comp_categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware','comp.sys.mac.hardware']
 rec_categories = ['rec.autos','rec.motorcycles','rec.sport.baseball','rec.sport.hockey']
 
+'''
 comp_train = fetch_20newsgroups(subset='train', categories=comp_categories, shuffle=True, random_state=42)
 comp_test = fetch_20newsgroups(subset='test', categories=comp_categories, shuffle=True, random_state=42)
 rec_train = fetch_20newsgroups(subset='train', categories=rec_categories, shuffle=True, random_state=42)
@@ -49,6 +50,7 @@ rec_test_list=rec_test.target.tolist()
 train_counts = [[comp_train_list.count(x)] for x in set(comp_train_list)] + [[rec_train_list.count(x)] for x in set(rec_train_list)]
 test_counts = [[comp_test_list.count(x)] for x in set(comp_test_list)] + [[rec_test_list.count(x)] for x in set(rec_test_list)]
 
+###TODO: Uncomment Before Submission ###
 objects = ('graphics', 'windows', 'ibm', 'mac', 'autos', 'cycles','baseball','hockey')
 y_pos = np.arange(len(objects))
 
@@ -58,7 +60,7 @@ plt.ylabel('Counts')
 plt.title('Counts for Training Set')
 plt.tight_layout()
 plt.savefig('training_set_histogram.png')
-plt.show()
+#plt.show()
 
 plt.bar(y_pos, np.array(test_counts), align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
@@ -66,23 +68,48 @@ plt.ylabel('Counts')
 plt.title('Counts for Test Set')
 plt.tight_layout()
 plt.savefig('test_set_histogram.png')
-plt.show()
+#plt.show()
+'''
 
 ### PART B ###
 def tokenize(data):
 
     stemmer = SnowballStemmer("english")
-    stop_words = text.ENGLISH_STOP_WORDS
     temp = data
-    regex = re.compile('[%s]' % re.escape(string.punctuation))
-    temp = regex.sub(' ', temp)
+    temp = "".join([a for a in temp if a not in set(string.punctuation)])
+    temp = re.sub('[,.-:/()?{}*$#&]', ' ', temp)
     temp = "".join(b for b in temp if ord(b) < 128)
-    temp = temp.lower()
     words = temp.split()
-    no_stop_words = [w for w in words if not w in stop_words]
-    stemmed = [stemmer.stem(item) for item in no_stop_words]
+    stemmed = [stemmer.stem(item) for item in words]
 
     return stemmed
+
+'''
+###TODO: Remove
+### This part is for testing ###
+corpus = [
+    'This is the first document.',
+    'This is the second22 second document.',
+    'And the third one.',
+    'Is this the first document?',
+    'He will play playing plays...',
+    'Walking I playing 5898 swimming',
+    'Thanks walks to school swim go play bee'
+]
+
+vectorizer = CountVectorizer(analyzer='word', stop_words='english', tokenizer=tokenize)
+tfidf_transformer = TfidfTransformer()
+X_train_counts = vectorizer.fit_transform(corpus)
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+print X_train_counts.shape
+print vectorizer.vocabulary_.keys()
+print X_train_tfidf.toarray()
+
+stop_words = text.ENGLISH_STOP_WORDS
+print list(stop_words).index("about")
+
+'''
 
 categories = comp_categories + rec_categories
 vectorizer = CountVectorizer(analyzer='word', stop_words='english', tokenizer=tokenize)
@@ -108,12 +135,14 @@ all_words = []
 all_word_freqs = []
 word_class_dict = defaultdict(list)
 
+"""
+
 for category in all_newsgroups:
     newsgroup_category = fetch_20newsgroups(subset='train', categories=[category], shuffle=True, random_state=42)
     newsgroup_category_data = newsgroup_category.data
     temp = ''
     for file in newsgroup_category_data:
-        temp += ' ' + file
+        temp = temp + file
     all_data.append(temp)
 
 for class_data,index in zip(all_data, range(len(all_data))):
@@ -129,17 +158,16 @@ for class_index in class_indices:
     terms_extracted_in_class = all_words[class_index]
     freq_of_terms_in_class = all_word_freqs[class_index]
     number_of_terms_extracted = len(terms_extracted_in_class)
-    maxFreq = max(freq_of_terms_in_class.values())
+
     tficf = dict()
-    
     for each_term in range(number_of_terms_extracted):
         term = terms_extracted_in_class[each_term]
         frequency_term = freq_of_terms_in_class.get(term)
         number_of_classes_with_term = len(word_class_dict[term])
-        tficf[term] = (0.5 + (0.5 * frequency_term/maxFreq)) * math.log(len(all_newsgroups)/number_of_classes_with_term)
+        tficf[term] = 0.5 + ((0.5 * frequency_term/number_of_terms_extracted) * math.log(len(all_newsgroups)/number_of_classes_with_term))
 
     print "Most significant 10 terms for class: " + str(all_newsgroups[class_index])
-    most_significant_terms = dict(sorted(tficf.iteritems(), key=operator.itemgetter(1), reverse=True)[:10])
+    most_significant_terms = dict(sorted(tficf.items(), key=operator.itemgetter(1), reverse=True)[:10])
     print (most_significant_terms.keys())
 """
 ### Part D: Dimension Reduction ###
@@ -163,8 +191,8 @@ X_test = min_max_scaler.transform(X_test_reduced)
 # by reducing all subclasses of comp and subclasses of rec
 # for train and test data => comp = 0, rec = 1
 
-train_targets = map(lambda x: int(x>=4), twenty_train.target.tolist())
-test_targets = map(lambda x: int(x>=4), twenty_test.target.tolist())
+#train_targets = map(lambda x: int(x>=4), twenty_train.target.tolist())
+#test_targets = map(lambda x: int(x>=4), twenty_test.target.tolist())
 """
 # Train model and predict labels for test set
 linear_SVM = LinearSVC(dual=False, random_state=42).fit(X_train, train_targets)
@@ -194,7 +222,7 @@ gamma_values = [10 ** i for i in range(-3,4)]
 
 for value in gamma_values:
     soft_margin_SVM = SVC(C=value, kernel='linear').fit(X_train, train_targets)
-    scores = cross_val_score(soft_margin_SVM, np.concatenate((X_train, X_test), axis=0),
+    scores = cross_validation.cross_val_score(soft_margin_SVM, np.concatenate((X_train, X_test), axis=0),
                                 np.append(train_targets, test_targets), cv=5, scoring='accuracy')
     accuracies.append(np.average(scores))
 
@@ -241,7 +269,7 @@ plt.savefig('mnnb_roc_curve.png')
 plt.show()
 
 ### Part H: Logistic Regression (Unregularized) ###
-logit = sk.LogisticRegression().fit(X_train, train_targets)
+logit = Logit(train_targets, X_train).fit()
 probabilities = logit.predict(X_test)
 predicted_lr = (probabilities > 0.5).astype(int)
 accuracy_lr = np.mean(predicted_lr == test_targets)
@@ -261,13 +289,10 @@ plt.legend(loc="lower right")
 plt.savefig('lru_roc_curve.png')
 plt.show()
 
+
 ### Part I: Logistic Regression (Regularized) ###
-coefs_1=[]
 for lam in [.0001,.001,.01,.5,1,10,100]:
     logit_r = sk.LogisticRegression(penalty='l1', C = float(1)/lam).fit(X_train, train_targets)
-
-    coefs_1.append(logit_r.coef_.ravel().copy())
-
     probabilities_r = logit_r.predict(X_test)
     predicted_lr_r = (probabilities_r > 0.5).astype(int)
     accuracy_lr_r = np.mean(predicted_lr_r == test_targets)
@@ -277,20 +302,8 @@ for lam in [.0001,.001,.01,.5,1,10,100]:
     print "Confusion Matrix:"
     print(confusion_matrix(test_targets, predicted_lr_r))
 
-coefs_1 = np.array(coefs_1)
-plt.plot(np.log10([.0001,.001,.01,.5,1,10,100]), coefs_1)
-ymin, ymax = plt.ylim()
-plt.xlabel('log(C)')
-plt.ylabel('Coefficients')
-plt.title('Logistic Regression Path')
-plt.axis('tight')
-plt.show()
-
-coefs_2=[]
 for lam in [.0001,.001,.01,.5,1,10,100]:
     logit_r = sk.LogisticRegression(penalty='l2', C = float(1)/lam).fit(X_train, train_targets)
-    coefs_2.append(logit_r.coef_.ravel().copy())
-    
     probabilities_r = logit_r.predict(X_test)
     predicted_lr_r = (probabilities_r > 0.5).astype(int)
     accuracy_lr_r = np.mean(predicted_lr_r == test_targets)
@@ -299,31 +312,9 @@ for lam in [.0001,.001,.01,.5,1,10,100]:
     print(classification_report(test_targets, predicted_lr_r))
     print "Confusion Matrix:"
     print(confusion_matrix(test_targets, predicted_lr_r))
-
-coefs_2 = np.array(coefs_2)
-plt.plot(np.log10([.0001,.001,.01,.5,1,10,100]), coefs_2)
-ymin, ymax = plt.ylim()
-plt.xlabel('log(C)')
-plt.ylabel('Coefficients')
-plt.title('Logistic Regression Path')
-plt.axis('tight')
-plt.show()
-
+"""
 ### Part J: Multinomial Naive Bayes and Linear Suppor Vector Machines with One vs. One and One vs. The Rest Classifiers ###
-categories = ['comp.sys.ibm.pc.hardware','comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
 
-twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-X_train_counts = vectorizer.fit_transform(twenty_train.data)
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-X_train_reduced = svd.fit_transform(X_train_tfidf)
-X_train = min_max_scaler.fit_transform(X_train_reduced)
-
-twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
-X_test_counts = vectorizer.transform(twenty_test.data)
-X_test_tfidf = tfidf_transformer.transform(X_test_counts)
-X_test_reduced = svd.transform(X_test_tfidf)
-X_test = min_max_scaler.transform(X_test_reduced)    
-    
 #Multinomial Naive Bayes
 clf = MultinomialNB().fit(X_train,  twenty_train.target.tolist())
 predicted_bayes = clf.predict(X_test)
@@ -333,6 +324,8 @@ print "Accuracy of Multinomial Naive Bayes: " + str(accuracy_bayes)
 print(classification_report( twenty_test.target.tolist(), predicted_bayes))
 print "Confusion Matrix:"
 print(confusion_matrix( twenty_test.target.tolist(), predicted_bayes))
+
+
 
 #One vs. One Linear SVM
 linear_SVM = OneVsOneClassifier(LinearSVC(dual=False, random_state=42)).fit(X_train, twenty_train.target.tolist())
